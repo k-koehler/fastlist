8d445f4129fad15145c8c370c855098c9751fbcd
Kevin <kevin.koehler@usask.ca>
2019-12-15 20:20:49 -0600

    optimized performanance for find operations

--- src/lib/cache.ts
+++ src/lib/cache.ts
@@ -1,5 +1,9 @@
 const DEFAULT_CACHE_MAX_SIZE = 1024;
-class Cache {
+class Cache<T> {
   private maxSize: number;
-  constructor(maxSize: number) {}
+  private cache: Map<number, T>;
+  constructor(maxSize?: number) {
+    this.cache = new Map();
+    this.maxSize = maxSize || DEFAULT_CACHE_MAX_SIZE;
+  }
 }
